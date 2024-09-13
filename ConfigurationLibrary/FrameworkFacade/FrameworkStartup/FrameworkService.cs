2be6324672b26ff607c532d59bf7f9cc73dc7757
Humberto Cruz <Humberto_Cruz@epam.com>
2024-09-12 19:01:55 -0600

    Framework facade initial version, IConfigurationService registered

---
+++ ConfigurationLibrary/FrameworkFacade/FrameworkStartup/FrameworkService.cs
@@ -0,0 +1,25 @@
+﻿using ConfigurationLibrary.Configuration;
+using ConfigurationLibrary.Interfaces.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FrameworkFacade.FrameworkStartup;
+
+public class FrameworkService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public FrameworkService(string filePath, string fileName)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(filePath);
+        ArgumentException.ThrowIfNullOrWhiteSpace(fileName);
+
+        _serviceProvider = new ServiceCollection()
+            .AddSingleton<IConfigurationService>(new ConfigurationService(filePath, fileName))
+            .BuildServiceProvider();
+    }
+
+    public IServiceProvider GetServiceProvider()
+    {
+        return _serviceProvider;
+    }
+}
--- ConfigurationLibrary/ReportPortal.ATM/Configuration/ConfigurationService.cs
+++ ConfigurationLibrary/ReportPortal.ATM/Configuration/ConfigurationService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using ConfigurationLibrary.Interfaces.Configuration;
+using Microsoft.Extensions.Configuration;
 using System;
 
 namespace ConfigurationLibrary.Configuration;
