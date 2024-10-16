9b078cf339e1dd0bd7cf0891f2b4b3a07d23c6cc
Humberto Cruz <Humberto_Cruz@epam.com>
2024-10-15 19:51:32 -0600

    Added POM

---
+++ ReportPortal.ATM/TestProject/Pages/AllDashboardPage/AllDashboardPage.locator.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestProject.Pages.AllDashboardPage;
+
+public partial class AllDashboardsPage
+{
+    private By _addNewDashboardLocator = By.XPath("//span[text()='Add New Dashboard']");
+    private By _editDashboardLocator = By.CssSelector(".icon__icon--coE7b.icon__icon-pencil--hZNP6");
+    private By _dashboardNamesLocator = By.XPath("//div[@class='gridRow__grid-row--X9wIq']//a");
+    private By _dashboardDescriptionsLocator = By.XPath("//div[@class='gridRow__grid-row--X9wIq']" +
+        "//div[@class='gridCell__grid-cell--EIqeC gridCell__align-left--DFXWN dashboardTable__description--tHp7Q']");
+
+    private IWebElement AddNewDashboardButton => _webDriver.FindElement(_addNewDashboardLocator);
+    //private IWebElement DashboardInfoElement(string name) =>
+    //_webDriver.FindElement(By.XPath($"//div[@class='gridRow__grid-row--X9wIq']//*[text()='{name}']"));
+
+    private IList<IWebElement> DashboardNames => _webDriver.FindElements(_dashboardNamesLocator).ToList();
+
+    private IList<IWebElement> DashboardDescriptions => _webDriver.FindElements(_dashboardDescriptionsLocator).ToList();
+
+    private IWebElement EditDashboardButton => _webDriver.FindElement(_editDashboardLocator);
+}
---
+++ ReportPortal.ATM/TestProject/Pages/AllDashboardPage/AllDashboardPage.private.cs
@@ -0,0 +1,72 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Linq;
+using WebDriverLibrary.Extensions.WebDrivers;
+
+namespace TestProject.Pages.AllDashboardPage;
+
+public partial class AllDashboardsPage
+{
+    private void ClickOnAddNewDashboard()
+    {
+        try
+        {
+            _webDriver.WaitUntilElementIsClickable(_addNewDashboardLocator,
+                _webDriverService.GetWebDriverConfiguration().LongTimeout,
+                _webDriverService.GetWebDriverConfiguration().PollingIntervalTimeout);
+
+            AddNewDashboardButton.Click();
+        }
+        catch (Exception e) { throw; }
+    }
+
+    private void ClickOnEditDashboard()
+    {
+        try
+        {
+            _webDriver.WaitUntilElementIsClickable(_editDashboardLocator,
+                _webDriverService.GetWebDriverConfiguration().LongTimeout,
+                _webDriverService.GetWebDriverConfiguration().PollingIntervalTimeout);
+
+            EditDashboardButton.Click();
+        }
+        catch (Exception e) { throw; }
+    }
+
+    private void ClickOnDashboard(string name)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+
+        try
+        {
+            FindDashboardName(name).Click();
+        }
+        catch (Exception e) { throw; }
+    }
+
+    private IWebElement FindDashboardName(string name)
+    {
+        try
+        {
+            _webDriver.WaitUntilElementIsVisible(_dashboardNamesLocator,
+                _webDriverService.GetWebDriverConfiguration().LongTimeout,
+                _webDriverService.GetWebDriverConfiguration().PollingIntervalTimeout);
+
+            return DashboardNames.FirstOrDefault(x => x.Text.Equals(name));
+        }
+        catch (Exception e) { throw; }
+    }
+
+    private IWebElement FindDashboardDescription(string description)
+    {
+        try
+        {
+            _webDriver.WaitUntilElementIsVisible(_dashboardDescriptionsLocator,
+                _webDriverService.GetWebDriverConfiguration().LongTimeout,
+                _webDriverService.GetWebDriverConfiguration().PollingIntervalTimeout);
+
+            return DashboardDescriptions.FirstOrDefault(x => x.Text.Equals(description));
+        }
+        catch (Exception e) { throw; }
+    }
+}
