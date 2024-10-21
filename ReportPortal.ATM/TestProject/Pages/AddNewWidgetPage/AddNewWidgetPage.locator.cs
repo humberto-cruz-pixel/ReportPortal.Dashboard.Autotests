c7faa5f02461b431a0dc3e320bf895a0eef211c9
Humberto Cruz <Humberto_Cruz@epam.com>
2024-10-21 13:34:58 -0600

    Adding feature file,steps and page for Add New Widget

---
+++ ReportPortal.ATM/TestProject/Pages/AddNewWidgetPage/AddNewWidgetPage.locator.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+
+namespace TestProject.Pages.AddNewWidgetPage;
+
+public partial class AddNewWidgetPage
+{
+    private By _nextStepButtonLocator = By.XPath("//span[text()='Next step']");
+    private By _defaultFilterSelector = By.ClassName("inputRadio__toggler--ygpdQ");
+    private By _widgetNameInputLocator = By.XPath("//input[@placeholder='Enter widget name']");
+    private By _addWidgetButtonLocator = By.XPath("//button[text()='Add']");
+    private By _widgetTypeLocator = By.CssSelector("widgetHeader__type--yZiVg");
+    private By _widgetNamesLocator = By.CssSelector(".widgetHeader__widget-name-block--AOAHS");
+
+    public IWebElement WidgetTypeInput(string type) => _webDriver.
+        FindElement(By.XPath($"//div[text()='{type}']/parent::span/preceding-sibling::span"));
+    public IWebElement NextStepButton => _webDriver.FindElement(_nextStepButtonLocator);
+    public IWebElement DefaultFilter => _webDriver.FindElement(_defaultFilterSelector);
+    public IWebElement WidgetNameInput => _webDriver.FindElement(_widgetNameInputLocator);
+    public IWebElement AddWidgetButton => _webDriver.FindElement(_addWidgetButtonLocator);
+    public IWebElement WidgetType=>_webDriver.FindElement(_widgetTypeLocator);
+}
---
+++ ReportPortal.ATM/TestProject/Pages/AddNewWidgetPage/AddNewWidgetPage.private.cs
@@ -0,0 +1,77 @@
+﻿using OpenQA.Selenium;
+using System;
+using WebDriverLibrary.Extensions.WebDrivers;
+
+namespace TestProject.Pages.AddNewWidgetPage;
+
+public partial class AddNewWidgetPage
+{
+    private void SelectWidget(string type)
+    {
+        try
+        {
+            WidgetTypeInput(type).Click();
+        }
+        catch (Exception e) { throw; }
+    }
+
+    private void ClickOneNextStepButton()
+    {
+        try
+        {
+            _webDriver.WaitUntilElementIsClickable(_nextStepButtonLocator,
+                _webDriverService.GetWebDriverConfiguration().LongTimeout,
+                _webDriverService.GetWebDriverConfiguration().PollingIntervalTimeout);
+
+            NextStepButton.Click();
+        }
+        catch (Exception e) { throw; }
+    }
+
+    private void ClickOnDefaultFilter()
+    {
+        try
+        {
+            _webDriver.WaitUntilElementIsClickable(_defaultFilterSelector,
+                _webDriverService.GetWebDriverConfiguration().LongTimeout,
+                _webDriverService.GetWebDriverConfiguration().PollingIntervalTimeout);
+
+            DefaultFilter.Click();
+        }
+        catch (Exception e) { throw; }
+    }
+
+    private void WidgetNameClearAndSendKeys(string name)
+    {
+        try
+        {
+            _webDriver.WaitUntilElementExists(_widgetNameInputLocator,
+                _webDriverService.GetWebDriverConfiguration().LongTimeout,
+                _webDriverService.GetWebDriverConfiguration().PollingIntervalTimeout);
+
+            WidgetNameInput.Clear();
+            WidgetNameInput.SendKeys(name);
+        }
+        catch (Exception e) { throw; }
+    }
+
+    private void ClickOnAddWidget()
+    {
+        try
+        {
+            _webDriver.WaitUntilElementIsClickable(_addWidgetButtonLocator,
+                _webDriverService.GetWebDriverConfiguration().LongTimeout,
+                _webDriverService.GetWebDriverConfiguration().PollingIntervalTimeout);
+
+            AddWidgetButton.Click();
+
+            Func<IWebDriver, bool> waitForWidgets = driver =>
+                driver.FindElements(_widgetNamesLocator).Count == _nWidget;
+
+            _webDriver.WaitForCondition(waitForWidgets,
+            _webDriverService.GetWebDriverConfiguration().LongTimeout,
+            _webDriverService.GetWebDriverConfiguration().PollingIntervalTimeout);
+        }
+        catch (Exception e) { throw; }
+    }
+}
