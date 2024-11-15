c530e5f73dadfe80350e9fd01d9cdbd95b191d49
Humberto Cruz <Humberto_Cruz@epam.com>
2024-11-14 18:58:13 -0600

    Added BaseTest Class, splitted tests into Services and Htpp methods

---
+++ ReportPortal.ATM/APITests/Tests/Dashboards/GetDashboardTests.cs
@@ -0,0 +1,74 @@
+﻿using APITests.Services;
+using System.Net;
+using System;
+
+namespace APITests.Tests.Dashboards;
+
+public class GetDashboardTests : BaseTest
+{
+
+    [Test]
+    public void Should_GetAllDashboards_usingHttpClient()
+    {
+        var response = new Dashboard(httpRestClientService).GetAllDashboards();
+        var data = response.GetData();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(response.StatusCode.Equals(HttpStatusCode.OK));
+            Assert.That(data.Page.Number > 0, "Should be at least 1 page");
+            Assert.That(data.Page.Size > 0, "Size can't be 0");
+            Assert.That(data.Page.TotalElements >= 0, "Total elements can't be less than 0");
+            Assert.That(data.Page.TotalPages >= 0, "Total pages can't be less than 0");
+        });
+    }
+
+    [Test]
+    public void Should_GetAllDashboards()
+    {
+        var response = dashboardService.GetAllDashboards();
+        var data = response.GetData();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(response.StatusCode.Equals(HttpStatusCode.OK));
+            Assert.That(data.Page.Number > 0, "Should be at least 1 page");
+            Assert.That(data.Page.Size > 0, "Size can't be 0");
+            Assert.That(data.Page.TotalElements >= 0, "Total elements can't be less than 0");
+            Assert.That(data.Page.TotalPages >= 0, "Total pages can't be less than 0");
+        });
+    }
+
+    [Test]
+    public void Should_GetDashboardById()
+    {
+        var name = Guid.NewGuid().ToString();
+
+        var id = dashboardService.CreateDashboard(name, "test")
+            .GetData().Id.ToString();
+
+        var response = dashboardService.GetDashboardById(id);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(response.StatusCode.Equals(HttpStatusCode.OK), $"Expected succesful status code, but found: {response.StatusCode}");
+            Assert.That(response.GetData().Name.Equals(name));
+
+        });
+
+        dashboardService.DeleteDashboardAsync(id);
+    }
+
+    [Test]
+    public void Should_GetDashboardByIdFailWhenIdNotFound()
+    {
+        var response = dashboardService.GetDashboardById("-1");
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound), $"Expected failed status code, but found: {response.StatusCode}");
+            Assert.That(response.GetData().Message.Equals("Dashboard with ID '-1' not found on project 'superadmin_personal'. Did you use correct Dashboard ID?"));
+
+        });
+    }
+}
---
+++ ReportPortal.ATM/APITests/Tests/Dashboards/PostDashboardTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System;
+using System.Linq;
+
+namespace APITests.Tests.Dashboards;
+
+public class PostDashboardTests : BaseTest
+{
+    [Test]
+    public void Should_CreateDashboard()
+    {
+        var name = Guid.NewGuid().ToString();
+        var description = Guid.NewGuid().ToString();
+
+        var response = dashboardService.CreateDashboard(name, description);
+        var id = response.GetData().Id;
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(response.StatusCode.Equals(HttpStatusCode.Created), $"Expected {HttpStatusCode.Created} status code, but found: {response.StatusCode}");
+            Assert.That(id, Is.Not.Null, "Id should not be empty");
+
+        });
+
+        var responseGetD = dashboardService.GetAllDashboards().GetData();
+
+        var Ids = responseGetD.Content.Select(x => x.Id).ToList();
+        var names = responseGetD.Content.Select(x => x.Name).ToList();
+        var descriptions = responseGetD.Content.Select(x => x.Description).ToList();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(Ids.Contains(id));
+            Assert.That(names.Contains(name));
+            Assert.That(descriptions.Contains(description));
+        });
+        dashboardService.DeleteDashboardAsync(id.ToString());
+    }
+
+    [Test]
+    public void Should_CreateDashboardFailWhenMissingName()
+    {
+        var description = Guid.NewGuid().ToString();
+
+        var response = dashboardService.CreateDashboard(" ", description);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(response.StatusCode, Is
+                .EqualTo(HttpStatusCode.BadRequest), $"Expected failed status code, but found: {response.StatusCode}");
+
+            Assert.That(response.GetData().Message, Is.
+                EqualTo("Incorrect Request. [Field 'name' should not contain only white spaces and shouldn't be empty. Field 'name' should have size from '3' to '128'.] "));
+
+        });
+    }
+}
